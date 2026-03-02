04627a07a2b373de2556f3ef528cbeb4e2cdd00f
Rajdeep Paul <rajdeeppaul427@gmail.com>
2026-03-02 20:57:00 +0530

    Refactor postfix evaluation logic for clarity

--- Day-6/Program to evaluate the given postfix expression.c
+++ Day-6/Program to evaluate the given postfix expression.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <ctype.h>
+
 int main() {
     char e[100];
-    int st[100], t=-1, i;
-    scanf("%s",e);
-    for(i=0;e[i]!='\0';i++) {
+    int st[100], t=-1;
+
+    printf("Enter postfix expression: ");
+    scanf("%s", e);
+
+    for(int i=0; e[i]!='\0'; i++) {
+
         if(isdigit(e[i])) {
-            st[++t]=e[i]-'0';
+            st[++t] = e[i] - '0';
         }
         else {
-            int b=st[t--];
-            int a=st[t--];
-            if(e[i]=='+') 
-                st[++t]=a+b;
-            else if(e[i]=='-') 
-                st[++t]=a-b;
-            else if(e[i]=='*') 
-                st[++t]=a*b;
-            else if(e[i]=='/') 
-                st[++t]=a/b;
+            int b = st[t--];
+            int a = st[t--];
+
+            switch(e[i]) {
+                case '+': st[++t] = a+b; break;
+                case '-': st[++t] = a-b; break;
+                case '*': st[++t] = a*b; break;
+                case '/': st[++t] = a/b; break;
+            }
         }
     }
-    printf("%d",st[t]);
+
+    printf("Result = %d", st[t]);
     return 0;
 }
