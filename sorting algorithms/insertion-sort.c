c41554a8398ef3d2fa039fafe95698f4daafd302
Rajdeep Paul <rajdeeppaul427@gmail.com>
2026-03-04 20:53:20 +0530

    Combine insertion sort and array printing in main

--- sorting algorithms/insertion-sort.c
+++ sorting algorithms/insertion-sort.c
@@ -1,41 +1,26 @@
 #include <stdio.h>
+int main() {
+    int arr[100], n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
 
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];    
-        j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
+        while(j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
-
-        arr[j + 1] = key; 
+        arr[j + 1] = key;
     }
-}
-
-void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++)
+    printf("Sorted array:\n");
+    for(int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
-    printf("\n");
-}
-
-int main() {
-    int arr[100], n, i;
-
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    insertionSort(arr, n);
-
-    printf("Ascending Order:\n");
-    printArray(arr, n);
-
+    }
     return 0;
 }
